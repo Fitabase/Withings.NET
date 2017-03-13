eb3e885cd95a104a78acf2c7c7992dd46e79d838
ValHalla358 <vcs358@gmail.com>
2017-03-13 12:43:55 -0700

    Create Testing for Skeleton
    Created Unit Testing

---
+++ WithingsTests/ProgramTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithingsTests
+{
+    [TestFixture]
+    public class ProgramTest
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
