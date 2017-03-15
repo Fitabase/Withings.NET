f91ce911f35302d7ea40a3f44a235f9388beb997
ValHalla358 <vcs358@gmail.com>
2017-03-15 09:38:18 -0700

    Revert "Revert "Ititialization Architecture""
    This reverts commit 6a756c6db831761648cb2b0b40c557cfc71c0ef4.

---
+++ Withings.SampleSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Withings.SampleSite.Startup))]
+namespace Withings.SampleSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
