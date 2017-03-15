8b8b98408f43331acd2cb462f3e3a295ac661f53
ValHalla358 <vcs358@gmail.com>
2017-03-15 09:35:09 -0700

    Adding Sample Project

--- Withings/Models/LifetimeTotals.cs
+++ Withings/Models/LifetimeTotals.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace Withings.Portable.Models
 {
     class LifetimeTotals
     {
+        [JsonProperty("heartRate")]
+        public int HeartRate { get; set; }
+        [JsonProperty("weight")]
+        public int Weight { get; set; }
     }
 }
